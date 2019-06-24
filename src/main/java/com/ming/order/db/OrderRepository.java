c779d6e5c8d6f88d8ca2c37d400bc3b4166d6b29
Chiminwon <chiminwon@gmail.com>
2019-06-24 10:18:56 +0800

    Initial Gradle Project

---
+++ src/main/java/com/ming/order/db/OrderRepository.java
@@ -0,0 +1,21 @@
+package com.ming.order.db;
+
+import com.ming.order.Order;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+public interface OrderRepository extends MongoRepository<Order, String> {
+
+    List<Order> findByCustomer(String customer);
+
+    List<Order> findByCustomerLike(String customer);
+
+    List<Order> findByCustomerAndType(String customer, String type);
+
+    List<Order> getByType(String type);
+
+    @Query("{customer:'Chuck Wagon'}")
+    List<Order> findChucksOrders();
+}
